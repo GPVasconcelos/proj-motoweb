29f12d0d1d2b798818704877c0c965c0a8cfb79b
Gholiveira6 <gustavoholiveira2005@gmail.com>
2025-06-16 22:06:16 -0300

    Atualizando Front-end

--- motoweb-app/src/services/api.ts
+++ motoweb-app/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://10.118.7.213:3000", // Troque pelo IP da sua máquina na rede
+  baseURL: "https://motoweb-api-production.up.railway.app",
 });
 
 export default api;
